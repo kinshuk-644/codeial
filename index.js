29bcd2513aba5e653a980b908aa241814b33d01d
Kinshuk <kinshuktaya644@gmail.com>
2021-07-20 15:57:40 +0530

    Setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const expressLayout = require("express-ejs-layouts");
 
 const port = 3000;
 
+app.use(express.static('./assets'));
+
+app.use(expressLayout);
+// extract style and scripts from sub pages into the layout 
+app.set('layout extractStyles', true); 
+app.set('layout extractScripts', true);
+
 // use express route for the routes 
 app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
     if(err){
