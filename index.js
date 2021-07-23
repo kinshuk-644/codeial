ce55ff66dcaa0daf4ad97b467a1d80bc047ff8f7
Kinshuk <kinshuktaya644@gmail.com>
2021-07-23 14:16:26 +0530

    Install express-session and use passport to authenticate while signing in

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 const port = 3000;
 
@@ -17,12 +20,26 @@ app.use(expressLayout);
 app.set('layout extractStyles', true); 
 app.set('layout extractScripts', true);
 
-// use express route for the routes 
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name: "Codeial",
+    // TODO change the secret key before deployment
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express route for the routes 
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
