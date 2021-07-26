d41eebdbd369504cbe13171e71425fbe5b4bf878
Kinshuk <kinshuktaya644@gmail.com>
2021-07-26 17:37:29 +0530

    Add functionality to delete a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = function(req, res){
     Post.create({
@@ -11,5 +12,24 @@ module.exports.create = function(req, res){
         }
 
         return res.redirect("back");
+    });
+};
+
+// delete a post and all its associated comments but the user can only delete the posts posted by him/her. 
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+
+        // use req.user.id instead of req.user._id to convert it into string and then compare
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect("back");
+            });
+        }
+
+        else{
+            return res.redirect("back");
+        }
     });
 };
