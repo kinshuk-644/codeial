93da7dabee3e4156cb0bcbaa99d1a863325d7716
Kinshuk <kinshuktaya644@gmail.com>
2021-08-02 15:50:25 +0530

    Set the destination and name for file uploads

--- models/user.js
+++ models/user.js
@@ -19,10 +19,22 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 } , {
     timestamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  });
 
 const User = mongoose.model('User', userSchema);
 
