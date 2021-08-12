8b8bfa162a045774c672a88e36fe76140fd512a8
Kinshuk <kinshuktaya644@gmail.com>
2021-08-12 12:55:58 +0530

    Create and join chat rooms

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -7,6 +7,16 @@ module.exports.chatSockets = function(socketServer){
         socket.on('disconnect', function(){
             console.log("socket disconnected");
         });
+
+        socket.on('join_room', function(data){
+            console.log("Joining request received ", data);
+
+            // make the user join the chat room 
+            socket.join(data.chatroom);
+
+            // tell only that room that a new user has joined 
+            io.in(data.chatroom).emit('user_joined', data);
+        });
     });
 
 
